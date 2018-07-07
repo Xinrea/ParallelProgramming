705bfe7b93f87542d71aac57b3678e7067b9b6ef
Xinrea <s11s@vip.qq.com>
2018-07-07 18:47:08 +0800

    update SEcreator

--- Lab2/MIP_pthread.cpp
+++ Lab2/MIP_pthread.cpp
@@ -2,28 +2,24 @@
 //Date: 2018/7/5
 
 #include "svpng.inc"
+#include "pngfunc.h"
 #include <cstdio>
-
-unsigned char* ArrayToRGB(int *a, int width, int height);
-int *RGBToArray(unsigned char *rgb, int width, int height);
-int *FileToArray(FILE* fp, int& width, int& height);
-
 /* Basic processing:
 File -(FileToArray)> Array -(MIP)> Array -(ArrayToRGB)> RGB -(svpng)> File */
 
 int main(int argc, char const *argv[])
 {
-    int array[5][5] = {{0,1,1,1,1},{0,1,0,1,0},{0},{0},{0}};
+    int array[6][6] = {{0,1,1,1,1,0},{0,1,0,1,0,1},{0},{0},{0},{0}};
     /* test ArrayToRGB */
-    unsigned char *rgb = ArrayToRGB((int*)array,5,5);
+    unsigned char *rgb = ArrayToRGB((int*)array,6,6);
     /* test RGBToArray */
-    int *a = RGBToArray(rgb,5,5);
-    for(int i = 0; i < 25; i++){
+    int *a = RGBToArray(rgb,6,6);
+    for(int i = 0; i < 36; i++){
         printf("%d ",a[i]);
     }
     printf("\n");
     FILE *fp = fopen("rgb.png", "wb");
-    svpng(fp, 5, 5, rgb, 0);
+    svpng(fp, 6, 6, rgb, 0);
     fclose(fp);
 
     fp = fopen("rgb.png","rb");
@@ -36,54 +32,4 @@ int main(int argc, char const *argv[])
     printf("\n");
     fclose(fp);
     return 0;
-}
-
-unsigned char* ArrayToRGB(int *a, int width, int height){
-    unsigned char *rgb = new unsigned char[width*height*3];
-    unsigned char *p = rgb;
-    for(int i = 0; i < height; i++){
-        for(int k = 0; k < width; k++){
-            if(*(a+i*width+k)){
-                *rgb++ = 0;
-                *rgb++ = 0;
-                *rgb++ = 0;
-            } else {
-                *rgb++ = 255;
-                *rgb++ = 255;
-                *rgb++ = 255;
-            }
-        }
-    }
-    return p;
-}
-
-int *RGBToArray(unsigned char *rgb, int width, int height){
-    int total = width*height;
-    int *a = new int[total];
-    int *p = a;
-    for(int i = 0; i < total; i++){
-        if(*(rgb+3*i)){
-            *a++ = 0;
-        } else {
-            *a++ = 1;
-        }
-    }
-    return p;
-}
-
-int *FileToArray(FILE* fp, int& width, int& height){
-    fseek(fp,0,SEEK_END);
-    int fsize = ftell(fp);
-    rewind(fp);
-    unsigned char *buffer = new unsigned char[fsize];
-    fread(buffer,1,fsize,fp);
-    width = buffer[19];
-    height = buffer[23];
-    int *res = new int[width*height];
-    for(int i = 0; i < height; i++){
-        for(int k = 0; k < width; k++){
-            res[i*width+k] = !(buffer[49+i*(width*3+1+5)+3*k]/255);
-        }
-    }
-    return res;
 }
--- Lab2/SEcreator.cpp
+++ Lab2/SEcreator.cpp
@@ -1,11 +1,30 @@
 //Author: Xinrea
 //Date: 2018/7/5
+//Tool program for creating structuring elements
 
 #include "svpng.inc"
+#include "pngfunc.h"
 #include <cstdio>
+#include <cstring>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf("Please Input File Name: ");
+    char filename[20];
+    scanf("%s",filename);
+    printf("Please Input Width and Height: ");
+    int width, height;
+    scanf("%d %d", &width, &height);
+    int *a = new int[width*height];
+    for(int i = 0; i < height; i++){
+        for(int k = 0; k < width; k++){
+            scanf("%d",&a[i*width+k]);
+        }
+    }
+    unsigned char *img = ArrayToRGB(a,width,height);
+    char fullname[40] = "Output/";
+    strcat(fullname,filename);
+    FILE *fp = fopen(fullname,"wb");
+    svpng(fp,width,height,img,0);
     return 0;
 }
